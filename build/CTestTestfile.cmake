fe0dbb6b9416a80178be6753d492a425878f3652
katyacher <katyacher@inbox.ru>
2024-02-13 15:12:16 +0300

    corrected project

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kate/skillbox/32_1_json
-# Build directory: /home/kate/skillbox/32_1_json/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/json-build")
--- build/_deps/json-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kate/skillbox/32_1_json/build/_deps/json-src
-# Build directory: /home/kate/skillbox/32_1_json/build/_deps/json-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
